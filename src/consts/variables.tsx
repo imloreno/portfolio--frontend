36ca9e72fc20f90f4872bdd95b555a255abec342
Lorenzo Arias Villegas <santyago.arias.sav@gmail.com>
2023-05-04 18:40:52 -0400

    Fixing styles and backend url

--- src/consts/variables.tsx
+++ src/consts/variables.tsx
@@ -1,4 +1,4 @@
-const MAIN_URL = "https://api.soylorenzo.tk/";
+const MAIN_URL = "https://imlorenotemp.000webhostapp.com";
 // const MAIN_URL = "http://localhost/projects/portfolio_backend/";
 
 export { MAIN_URL };
